ef5f072b7b7de24f7b69179ddc4ef21ded8f5bea
Umang Agarwal <46571828+umangagarwal11@users.noreply.github.com>
2021-07-03 05:15:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA_GvJZvAo8wxZVtkz6En5WYTojPttaQCg",
-    authDomain: "chatapp-dd365.firebaseapp.com",
-    projectId: "chatapp-dd365",
-    storageBucket: "chatapp-dd365.appspot.com",
-    messagingSenderId: "1026164055229",
-    appId: "1:1026164055229:web:bfd345fba2c139cf71ebd4",
-    measurementId: "G-X3J615TKGP"
+    //Your firebase project web config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
